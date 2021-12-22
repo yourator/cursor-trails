8d790a348065bd5c1acb0989ad55087c5b190df6
fin <finfin@gmail.com>
2021-12-23 06:17:05 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,116 +1,221 @@
+const IMAGE_URLS = [
+  'https://i.imgur.com/ATIK70z.png',
+  'https://i.imgur.com/j1Ji19r.png',
+  'https://i.imgur.com/7OONoPN.png',
+  'https://i.imgur.com/CVUTdtG.png',
+  'https://i.imgur.com/YOxgfxw.png',
+  'https://i.imgur.com/55Vsqji.png',
+  'https://i.imgur.com/rP46Ovp.png',
+  'https://i.imgur.com/KYoL6oi.png'
+]
 
-const DEFAULT_RATE = 0.15 // 雪花頻率
-const SIZE = '3rem'
-
-const IMAGE_URLS = []
 
 function loadImage(url) {
-  const imageElement = new Image()
-  imageElement.src = url
-  imageElement.style.width = SIZE
-  imageElement.style.height = SIZE
+  return new Promise(((resolve) => {
+    const img = new Image()
 
-  return imageElement
+    img.onload = function onloadHandler() {
+      resolve(img)
+    }
+
+    img.onerror = () => resolve(null)
+    img.src = url
+  }))
 }
 
-function getRandomItem(items) {
-  return items[Math.floor(Math.random() * items.length)]
-}
-
-function setImageAnimation(imageElement, container) {
-  imageElement.style.transform = `translate(
-    ${(0.4 * Math.random() - 0.2) * container.clientHeight}px,
-    ${(0.4 * Math.random() + 0.4) * container.clientHeight}px) scale(0.1) rotate(${1080 * Math.random() - 540}deg)`
-  imageElement.style.opacity = 0
-
-  return imageElement
-}
-
-function getRelativePosition(position, container) {
-  const rect = container.getBoundingClientRect()
+function getRelativePosition(position, canvas) {
+  const rect = canvas.getBoundingClientRect()
   const x = position.x - rect.left
   const y = position.y - rect.top
   return { x, y }
 }
-function getImage(imageElements, startingPosition, container) {
-  const displayImageElement = getRandomItem(imageElements).cloneNode(true)
-  const relativePosition = getRelativePosition(startingPosition, container)
-  displayImageElement.style.position = 'absolute'
-  displayImageElement.style.left = `${relativePosition.x}px`
-  displayImageElement.style.top = `${relativePosition.y}px`
-  displayImageElement.style.transition = `transform ${1 * Math.random() + 2}s linear,
-  opacity ${1 * Math.random() + 1.5}s linear`
-  displayImageElement.style.pointerEvents = 'none'
-  displayImageElement.addEventListener('transitionend', () => {
-    if (displayImageElement.parentNode) {
-      displayImageElement.parentNode.removeChild(displayImageElement)
+
+function snowflakeCursor({
+  container = document.body,
+  images = IMAGE_URLS, // 圖片 url
+  rate = 1, // 雪花出現頻率
+  size = 30, // 雪花大小
+  life = 2, // 雪花消失時間
+  speed = 0.5, // 雪花移動速度
+} = {}) {
+  const hasWrapperEl = container !== document.body
+
+  const particles = []
+  let canvas
+  let context
+  let canvasImages = []
+
+
+  /**
+   * Particles
+   */
+
+  function Particle(x, y, img) {
+    const lifeSpan = Math.floor(Math.random() * 60 + 80) * life
+    this.initialLifeSpan = lifeSpan //
+    this.lifeSpan = lifeSpan // ms
+    this.velocity = {
+      x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2) * speed,
+      y: (1 + Math.random()) * speed,
     }
-  })
-  return displayImageElement
-}
+    this.position = { x, y }
+    // var img = new Image();   // Create new img element
+    // img.src = canvasItem;
+    img.width = size
+    img.height = size
+    this.trailImage = img
 
+    this.update = (_context) => {
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y
+      this.lifeSpan--
 
-function CursorTrail(options) {
-  const container = (options && options.container) || document.body
-  const rate = (options && options.rate) || DEFAULT_RATE
-  const images = options.images || IMAGE_URLS
-  const imageElements = images.map(loadImage)
+      this.velocity.x += ((Math.random() < 0.5 ? -1 : 1) * 2) / 75
+      // this.velocity.y -= Math.random() / 300
 
-  if (!imageElements || !imageElements.length) {
-    throw new Error('No image elements provided')
+      const scale = Math.max(this.lifeSpan / this.initialLifeSpan, 0)
+
+      const degrees = 2 * this.lifeSpan
+      const radians = degrees * 0.0174533 // not perfect but close enough
+
+      _context.save()
+      _context.globalAlpha = this.lifeSpan / this.initialLifeSpan
+      _context.translate(this.position.x, this.position.y)
+      _context.rotate(radians)
+      _context.drawImage(
+        this.trailImage,
+        (-this.trailImage.width / 2) * scale,
+        -this.trailImage.height / 2,
+        this.trailImage.width * scale,
+        this.trailImage.height * scale,
+      )
+
+      _context.restore()
+    }
+
+    this.update(context)
+  }
+  function addParticle(x, y) {
+    if (rate > Math.random()) {
+      particles.push(new Particle(x, y, canvasImages[Math.floor(Math.random() * canvasImages.length)]))
+    }
   }
 
-  function addTrails(x, y) {
-    const image = getImage(imageElements, { x, y }, container)
-    container.appendChild(image)
-    setImageAnimation(image, container)
-  }
+  function updateParticles() {
+    // console.log(particles.length)
+    let removeParticle = false
+    if (particles.length === 0) { return }
+    const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
+    // Update
 
+    if (!reducedMotion) {
+      context.clearRect(0, 0, canvas.width, canvas.height)
 
-  function onTouchMove(e) {
-    if (e.touches.length > 0) {
-      for (let i = 0; i < e.touches.length; i += 1) {
-        if (rate > Math.random()) {
-          requestAnimationFrame(() => {
-            addTrails(
-              e.touches[i].clientX,
-              e.touches[i].clientY,
-            )
-          })
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].update(context)
+      }
+
+      // Remove dead particles
+    } else {
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].lifeSpan = particles[i].lifeSpan - 1
+
+        if (particles[i].lifeSpan < 0) {
+          particles.splice(i, 1)
+          removeParticle = true
+        }
+      }
+
+      if (removeParticle) {
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        for (let i = 0; i < particles.length; i++) {
+          particles[i].update(context)
         }
       }
     }
+    // for (let i = particles.length - 1; i >= 0; i--) {
+    //   if (particles[i].lifeSpan < 0) {
+    //     particles.splice(i, 1)
+    //   }
+    // }
   }
-  function onTouchStart(e) {
+  function onWindowResize(e) {
+    if (hasWrapperEl) {
+      canvas.width = container.clientWidth
+      canvas.height = container.clientHeight
+    } else {
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    }
+  }
+
+  function onTouchMove(e) {
     if (e.touches.length > 0) {
-      requestAnimationFrame(() => {
-        addTrails(
-          e.touches[0].clientX,
-          e.touches[0].clientY,
-        )
-      })
+      for (let i = 0; i < e.touches.length; i++) {
+        const { x, y } = getRelativePosition({ x: e.touches[i].clientX, y: e.touches[i].clientY }, canvas)
+
+        addParticle(x, y)
+      }
     }
   }
 
   function onMouseMove(e) {
-    if (rate > Math.random()) {
-      requestAnimationFrame(() => {
-        addTrails(
-          e.clientX,
-          e.clientY,
-        )
-      })
-    }
+    const { x, y } = getRelativePosition({ x: e.clientX, y: e.clientY }, canvas)
+    console.log('mm')
+    addParticle(x, y)
   }
 
   // Bind events that are needed
   function bindEvents() {
-    container.addEventListener('mousemove', onMouseMove)
-    container.addEventListener('touchmove', onTouchMove)
-    container.addEventListener('touchstart', onTouchStart)
+    const isTouchDevice = window.matchMedia('(pointer: coarse)').matches
+
+    if (isTouchDevice) {
+      container.addEventListener('touchmove', onTouchMove)
+      container.addEventListener('touchstart', onTouchMove)
+    } else {
+      container.addEventListener('mousemove', onMouseMove)
+    }
+    window.addEventListener('resize', onWindowResize)
   }
 
-  bindEvents()
+  function loop() {
+    updateParticles()
+    requestAnimationFrame(loop)
+  }
+  function init() {
+    canvas = document.createElement('canvas')
+    context = canvas.getContext('2d')
+
+    canvas.style.top = '0px'
+    canvas.style.left = '0px'
+    canvas.style.pointerEvents = 'none'
+
+    if (hasWrapperEl) {
+      canvas.style.position = 'absolute'
+      container.appendChild(canvas)
+      container.style.position = 'relative'
+      canvas.width = container.clientWidth
+      canvas.height = container.clientHeight
+    } else {
+      canvas.style.position = 'fixed'
+      document.body.appendChild(canvas)
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    }
+
+
+    bindEvents()
+    loop()
+  }
+
+  Promise.all(images.map(url => loadImage(url))).then((results) => {
+    canvasImages = results.filter(result => result !== null)
+
+    if (results.length) {
+      init()
+    }
+  })
 }
 
-export default CursorTrail
+
+export default snowflakeCursor
